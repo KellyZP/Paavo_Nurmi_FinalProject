542edca0de9f28bb5b4e1b1bf77be82d80d64ffe
fishe2jo <fishe2jo@mail.uc.edu>
2023-04-18 17:38:01 -0400

    Completion of Step 1 and 2 for the final project (Jake Fisher and Zack Kelly.

--- Paavo_Nurmi_FinalProject/functionsPackage/functions.py
+++ Paavo_Nurmi_FinalProject/functionsPackage/functions.py
@@ -1,20 +1,28 @@
-#functions.py
+# Group Name: Paavo Nurmi (Gavin Reinhard, Sarah Ouellette, Zach Kelly, & Jakob Fisher)
+# Email: ,,,fishe2jo@mail.uc.edu
+# Assignment Title: Final Project
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: Function module that provides the functions to decrypt the location data and display our team photo.
+# Citations: https://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
+# Anything else that's relevant: 
+# functions.py
 
 import json
 
-def read_JSON():
-    with open('EncryptedGroupHints Spring 2023 Section 001.json', 'r') as f:
+def decrypt_message(team_name,dict_path,encrypted_path):
+    with open(encrypted_path, 'r') as f:
         temp = f.read()
     data = json.loads(temp)
-    values = [value for key, value in data.items()]
-    print(values)
-
-
-
-def load_file(file_path):
-    try:
-        tmp_lines = [line.strip().lower() for line in open(file_patj)]
-    except:
-        tmp_lines = None
-    return tmp_lines
+    message = data[team_name]
+    
+    english = []
+    with open(dict_path) as file:
+        while line := file.readline():
+            english.append(line.rstrip())
+            
+    location = ''
+    for item in message:
+        location += english[int(item) - 1] + ' '
+    return location
     
--- Paavo_Nurmi_FinalProject/mainPackage/main.py
+++ Paavo_Nurmi_FinalProject/mainPackage/main.py
@@ -1,8 +1,14 @@
+# Group Name: Paavo Nurmi (Gavin Reinhard, Sarah Ouellette, Zach Kelly, & Jakob Fisher)
+# Email: ,,,fishe2jo@mail.uc.edu
+# Assignment Title: Final Project
+# Course: IS 4010
+# Semester/Year: Spring 2023
+# Brief Description: Main module that invokes the functions from the function package.
+# Citations:
+# Anything else that's relevant: 
 # main.py
 
 from functionsPackage.functions import *
-import json
 
-read_JSON()
-
-print(load_file("english.txt"))
+decryptedData = decrypt_message('Paavo Nurmi','english.txt','EncryptedGroupHints Spring 2023 Section 001.json')
+print(decryptedData)
